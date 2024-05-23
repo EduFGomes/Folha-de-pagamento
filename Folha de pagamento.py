90d17c74093d4f25ecd098948986446cdd1210db
lasuzukii <144397153+lasuzukii@users.noreply.github.com>
2024-05-23 14:50:13 -0300

    Update Folha de pagamento.py

--- Folha de pagamento.py
+++ Folha de pagamento.py
@@ -1,16 +1,18 @@
 def menu():
-    print('-' * 50)
+    print(f'-' * 50)
     print('Bem vindo ao sistema de folha de pagamento da Marketing é Tudo!')
-    print('')
-    print('Para utilizar todas as funcionalidades, acesse o menu abaixo:')
-    print('Digite 1 para inserir um funcionário;')
-    print('Digite 2 para remover um funcionário;')
-    print('Digite 3 para mostrar a folha de pagamento de um funcionário;')
-    print('Digite 4 para mostrar o relatório de todos os funcionários;')
-    print('Digite 5 para mostrar o funcionário com maior salário ;')
-    print('Digite 6 para mostrar o funcionário com mais faltas;')
-    print('Digite outro número para finalizar o programa.')
-    print('')
+    print(f'-' * 50)
+    print('Digite o número da opção desejada:')
+    print(f'-' * 50)
+    print('1 - INSERIR FUNCIONÁRIO;')
+    print('2 - REMOVER FUNCIONÁRIO;')
+    print('3 - FOLHA DE PAGAMENTO DE UM FUNCIONÁRIO;')
+    print('4 - RELATÓRIO DE TODOS OS FUNCIONÁRIOS;')
+    print('5 - MOSTRAR FUNCIONÁRIO COM MAIOR SALÁRIO ;')
+    print('6 - MOSTRAR O FUNCIONÁRIO COM MAIS FALTAS;')
+    print('OUTRO NÚMERO PARA FINALIZAR O PROGRAMA.')
+    print(f'-' * 50)
+    print()
 
     resp = int(input())
     if resp == 1:
@@ -31,29 +33,37 @@ def inserir():
     print('')
     print('1 - INSERIR FUNCIONÁRIO')
     print('-' * 50)
+    
     matricula = int(input('Digite o número de matrícula: '))
     while matricula in funcionarios.keys():
         print('Matrícula já existente. Insira outro número.')
         matricula = int(input('Digite o número de matrícula: '))
+    
     nome = input('Digite o nome do funcionário: ')
+    
     cod_func = int(input('Digite o código da função do funcionário: '))
     while cod_func != 101 and cod_func != 102:
         print('O código inserido é invalido. Tente novamente.')
         cod_func = int(input('Digite o código da função do funcionário: '))
+    
     num_falta = int(input('Digite a quantidade de faltas do funcionário no mês: '))
     while num_falta > 30:
         print('Número de faltas excedeu o máximo permitido. Tente novamente.')
         num_falta = int(input('Digite a quantidade de faltas do funcionário no mês: '))
     if cod_func == 101:
-        sal_bruto = 1500
+        sal_fixo = 1500
         vol_vendas = float(input('Digite o valor do volume de vendas do funcionário: '))
-        sal_bruto = sal_bruto + (vol_vendas * 0.09)
+        sal_bruto = sal_fixo + (vol_vendas * 0.09)
     else:
-        sal_bruto = float(input('Digite o salário do funcionário: '))
-        while sal_bruto < 2150 or sal_bruto > 6950:
+        sal_fixo = float(input('Digite o salário do funcionário: '))
+        while sal_fixo < 2150 or sal_fixo > 6950:
             print('Valor inválido. Tente novamente.')
-            sal_bruto = float(input('Digite o salário do funcionário: '))
-    sal_bruto = sal_bruto - (sal_bruto/30 * num_falta)
+            sal_fixo = float(input('Digite o salário do funcionário: '))
+    
+    desc_faltas = ((sal_fixo/30)* num_falta)
+    
+    sal_bruto = sal_bruto - desc_faltas
+    
     if sal_bruto <= 2259.20:
         sal_liq = sal_bruto
         imposto = 0
@@ -69,7 +79,7 @@ def inserir():
     elif 4664.68 < sal_bruto:
         sal_liq = sal_bruto - (sal_bruto * 0.275)
         imposto = 27.5
-    funcionarios[matricula] = [nome.title(), cod_func, num_falta, sal_bruto, imposto, sal_liq]
+    funcionarios[matricula] = [nome.title(), cod_func, num_falta, desc_faltas, sal_bruto, imposto, sal_liq]
     print('')
     print('FUNCIONÁRIO INSERIDO.')
     print('')
@@ -89,7 +99,7 @@ def remover():
     matricula = int(input('Digite o número da matrícula do funcionário que deseja remover: '))
     if matricula in funcionarios.keys():
         print(funcionarios[matricula][0])
-        nome = input('Para confirmar, digite o nome do funcionário completo: ')
+        nome = input('Para confirmar, digite o primeiro nome do funcionário: ')
         if nome.title() == funcionarios[matricula][0]:
             func_remov = funcionarios.pop(matricula)
             print(f'O FUNCIONÁRIO {func_remov[0].upper()} FOI REMOVIDO.')
@@ -125,9 +135,10 @@ def folha_de_pag():
         print(f'Nome do funcionário:   {funcionarios[matricula][0]}')
         print(f'Código do funcionário: {funcionarios[matricula][1]}')
         print(f'Número de faltas:      {funcionarios[matricula][2]}')
-        print(f'Salário bruto:         {funcionarios[matricula][3]}')
-        print(f'Imposto:               {funcionarios[matricula][4]}%')
-        print(f'Salário líquido:       {funcionarios[matricula][5]}')
+        print(f'Desconto das faltas:   {funcionarios[matricula][3]}')
+        print(f'Salário bruto:         {funcionarios[matricula][4]}')
+        print(f'Imposto:               {funcionarios[matricula][5]}%')
+        print(f'Salário líquido:       {funcionarios[matricula][6]}')
         print('')
 
     frase = 'mostrar a folha de pagamento de mais algum funcionário'
@@ -142,8 +153,8 @@ def relatorio():
         print(f'Número de Matrícula: {matricula}')
         print(f'Nome: {lista[0]}')
         print(f'Códigoda função: {lista[1]}')
-        print(f'Salário bruto: {lista[3]}')
-        print(f'Salário líquido: {lista[5]}')
+        print(f'Salário bruto: {lista[4]}')
+        print(f'Salário líquido: {lista[6]}')
         print('')
 
     frase = 'realizar o relatório novamente'
@@ -153,30 +164,37 @@ def relatorio():
 def maiorsal():
     maior_s = 0
     for i in funcionarios.keys():
-        if funcionarios[i][5] > maior_s:
-            maior_s = funcionarios[i][5]
+        if funcionarios[i][6] > maior_s:
+            maior_s = funcionarios[i][6]
             matricula = funcionarios[i]
-    print('funcionário com o maior salário:')
+    print('MAIOR SALÁRIO:')
     print(f'Número de matrícula: {matricula}')
     print(f'Nome do funcionário: {matricula[0]}')
     print(f'Código do funcionário: {matricula[1]}')
-    print(f'Salário bruto: {matricula[3]}')
-    print(f'Imposto: {matricula[4]}%')
-    print(f'Salário líquido: {matricula[5]}')
+    print(f'Salário bruto: {matricula[4]}')
+    print(f'Imposto: {matricula[5]}%')
+    print(f'Salário líquido: {matricula[6]}')
+    
+    frase = 'mostrar o funcionário com maior salário novamente'
+    func = 5
+    voltar(frase, func)
 
 def maisfaltas():
     mais_f = 0
     for i in funcionarios.keys():
         if funcionarios[i][2] > mais_f:
             mais_f = funcionarios[i][2]
-            desconto = (funcionarios[i][3] / 30) * funcionarios[i][2]
             matricula = funcionarios[i]
-    print('Funcionário com mais faltas:')     
+    print('MAIOR NÚMERO DE FALTAS:')     
     print(f'Número de matrícula: {matricula}')
     print(f'Nome do funcionário: {matricula[0]}')
     print(f'Código do funcionário: {matricula[1]}')
     print(f'Número de faltas: {matricula[2]}')
-    print(f'Desconto no salário: {desconto}')
+    print(f'Desconto das faltas: {matricula[3]}')
+    
+    frase = 'mostrar o funcionário com maior número de faltas novamente'
+    func = 6
+    voltar(frase, func)
 
 def voltar(frase, func):
     print(f'Digite 1 se você deseja {frase};')
@@ -197,9 +215,9 @@ def voltar(frase, func):
             folha_de_pag()
         elif func == 4:
             relatorio()
-        elif resp == 2:
-            menu()
+    elif resp == 2:
+        menu()
 
-funcionarios = {1: ['João', 101, 0, 1000.00, 0, 1590.00], 39: ['Jorge', 101, 0, 1000.00, 0, 1590.00]}
+funcionarios = {1: ['João', 101, 0, 0, 1000.00, 0, 1591.00], 39: ['Jorge', 101, 0, 0, 1000.00, 0, 1590.00]}
 menu() 
 print('Obrigado por acessar o programa!')
